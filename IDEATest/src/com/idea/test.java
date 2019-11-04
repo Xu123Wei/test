f278b4a136dcfe1c6a8206e2c9cd27a5946937df
Xuwei <xu123wei@163.com>
2019-11-04 13:08:40 +0800

    第一次提交

---
+++ IDEATest/src/com/idea/test.java
@@ -0,0 +1,11 @@
+package com.idea;
+/*
+@author:Xuwei
+@date: 2019/11/4  -  11:30
+*/
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("欢迎使用idea");
+    }
+}
